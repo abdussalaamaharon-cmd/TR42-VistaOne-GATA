61f7df1bad7baf7a5e3b4a9c333eebddb71b4da3
Dhanushka <dhanushkamcr@gmail.com>
2026-03-23 13:54:13 -0500

    Initialize user model

--- vistaone-api/app/models.py
+++ vistaone-api/app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import DateTime, func
 
 class Base(DeclarativeBase):
     pass
@@ -10,3 +11,11 @@ class User(Base):
     __tablename__ = 'users'
     
     id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    last_name: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    username: Mapped[str] = mapped_column(db.String(255), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(db.String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    role_id: Mapped[int] = mapped_column(nullable=False)
+    company_id: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
